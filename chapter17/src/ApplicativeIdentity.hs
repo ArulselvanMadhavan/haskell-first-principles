1ca990540ad70714718826dca6634979c3b7baa2
Madhavan, Arulselvan <arulselvan1234@gmail.com>
2018-02-13 06:49:18 -0800

    Applicative for Identity type

---
+++ chapter17/src/ApplicativeIdentity.hs
@@ -0,0 +1,12 @@
+module ApplicativeIdentity where
+
+newtype Identity a =
+  Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure a = Identity a
+  (<*>) (Identity f) (Identity a) = Identity (f a)
