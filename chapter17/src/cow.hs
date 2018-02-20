15a27161fd9c23e783357d1260fbd4e332c3cea1
Madhavan, Arulselvan <arulselvan1234@gmail.com>
2018-02-19 18:32:47 -0800

    Ch 17 - Cow Exercise

---
+++ chapter17/src/cow.hs
@@ -0,0 +1,29 @@
+module Cow where
+
+data Cow = Cow
+  { name   :: String
+  , age    :: Int
+  , weight :: Int
+  } deriving (Eq, Show)
+
+noEmpty :: String -> Maybe String
+noEmpty ""  = Nothing
+noEmpty str = Just str
+
+noNegative :: Int -> Maybe Int
+noNegative n
+  | n >= 0 = Just n
+  | otherwise = Nothing
+
+cowFromString :: String -> Int -> Int -> Maybe Cow
+cowFromString n a w =
+  case noEmpty n of
+    Nothing -> Nothing
+    Just nn ->
+      case (noNegative a, noNegative w) of
+        (Just aa, Just ww) -> Just (Cow n aa ww)
+        (_, _)             -> Nothing
+
+cowFromString' :: String -> Int -> Int -> Maybe Cow
+cowFromString' n a w =
+    Cow <$> noEmpty n <*> noNegative a <*> noNegative w
