5c46bf6359fa8e34ac1b15b7e6d3c6e0e95ae3be
Arul Madhavan <arulselvan1234@gmail.com>
2018-04-17 05:45:47 -0700

    Ch 24.5

--- chapter24/src/Text.Fractions.hs
+++ chapter24/src/Text.Fractions.hs
@@ -18,6 +18,14 @@ parseFraction = do
   denom <- decimal
   return (num % denom)
 
+virtuousFraction :: Parser Rational
+virtuousFraction = do
+  num <- decimal
+  char '/'
+  denom <- decimal
+  case denom of
+    0 -> fail "Denom can't be zero" -- fail is part of Monad typeclass.
+    _ -> return (num % denom)
 
 main :: IO ()
 main = do
@@ -26,3 +34,23 @@ main = do
   print $ parseFraction' shouldAlsoWork
   print $ parseFraction' alsoBad
   print $ parseFraction' badFraction
+
+testVirtuous :: IO ()
+testVirtuous = do
+  let parseFraction' = parseString virtuousFraction mempty
+  print $ parseFraction' alsoBad
+  print $ parseFraction' badFraction
+  print $ parseFraction' shouldWork
+  print $ parseFraction' shouldAlsoWork
+
+successInteger :: Parser Integer
+successInteger = do
+  i <- integer
+  eof
+  return i
+
+parseInteger :: IO ()
+parseInteger = do
+  let parseFraction' = parseString successInteger mempty
+  print $ parseFraction' "123abc"
+  print $ parseFraction' "12345"
