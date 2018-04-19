55271c481f77787bb6a745ec505304c73672af54
Arul Madhavan <arulselvan1234@gmail.com>
2018-04-18 21:28:49 -0700

    Ch 24 - QuasiQuotes continued

--- chapter24/src/QuasiModo.hs
+++ chapter24/src/QuasiModo.hs
@@ -1,15 +1,9 @@
 {-# LANGUAGE QuasiQuotes #-}
-module AltParsing where
 
+module QuasiModo where
 import           Control.Applicative
 import           Text.RawString.QQ
 import           Text.Trifecta
-
-type NumberOrString = Either Integer String
-
-a = "blah"
-b = "123"
-c = "123blah789"
 
 eitherOr :: String
 eitherOr = [r|
@@ -19,17 +13,13 @@ abc
 def
 |]
 
+
+type NumberOrString = Either Integer String
+
 parseNos :: Parser NumberOrString
 parseNos = (Left <$> integer) <|> (Right <$> some letter)
 
 main :: IO ()
 main = do
   let p f i = parseString f mempty i
-  let res1 :: Result [Integer]
-      res1 = p (some integer) "12893129817123"
-  print $ p (some letter) a
-  print $ p integer b
-  print $ p parseNos a
-  print $ p (many parseNos) c
-  print $ p (some parseNos) c
-  print $ res1
+  print $ p parseNos eitherOr
