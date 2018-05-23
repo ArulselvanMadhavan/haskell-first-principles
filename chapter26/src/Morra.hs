ce520000691b3c20d488cce5eb520b3ca483e9a3
Arul Madhavan <arulselvan1234@gmail.com>
2018-05-22 19:08:22 -0700

    Ch26 - Morra in Progress

--- chapter26/src/Morra.hs
+++ chapter26/src/Morra.hs
@@ -3,24 +3,24 @@ import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Maybe
+import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.State
 import           System.Random
 -- StateT should save scores of both player and computer
 -- Computer should choose its hand randomly
 -- When the game exits, it should report the winner
 
-data Winner
-  = Human
-  | AI
-  deriving (Eq, Show)
+newtype Human = Human Int
+newtype AI = AI Int
+
 
 data Input = Input
   { value :: Int
   , guess :: Int
   } deriving (Eq, Show)
 
-type Guess = (Int, Int)
-
+type Score = (Human, AI)
+type Guess = (Input, Input)
 
 readValue :: (Read a) => IO a
 readValue = fmap read getLine
@@ -38,7 +38,7 @@ checkQuitAndContinue s         = Just s
 stringToInt :: Maybe String -> Maybe Int
 stringToInt = fmap read
 
-go :: MaybeT IO (Input, Input)
+go :: MaybeT IO Guess
 go = do
   liftIO $ putStr "Enter your finger count(q-quit):"
   h1 <- MaybeT (fmap (stringToInt . checkQuitAndContinue) getLine)
@@ -48,11 +48,31 @@ go = do
   c2 <- liftIO $ readAiInput 10
   return (Input h1 h2, Input c1 c2)
 
-main :: IO Int
-main = evalStateT go' []
+guessToSum :: Guess -> Int
+guessToSum = (+) <$> (value . fst) <*> (value . snd)
 
-go' :: StateT [Guess] IO Int
+getScore :: Int -> Score
+getScore s
+    | s < 0 = (Human 1, AI 0)
+    | otherwise = (Human 0, AI 0)
+
+toResult :: MaybeT IO Score
+toResult = do
+    g <- go
+    let s = guessToSum g
+    let h = snd g
+    let c = snd g
+    return s
+        -- | h == sum = (Human 1, AI 0)
+        -- | c == sum = (Human 0, AI 1)
+        -- | otherwise = (Human 0, AI 0)
+
+main :: IO Int
+main = evalStateT go' (Human 0, AI 0)
+
+go' :: StateT Score IO Int
 go' = do
   s <- get
-  ss <- liftIO $ go s
+  let ir = go
+  ss <- liftIO $ go
   return $ length ss
